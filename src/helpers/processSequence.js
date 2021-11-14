0de660eaee3badf5f77344715be09c5a6f01139e
Alexander Mamutov <a.mamutov@maugry.ru>
2021-11-14 16:19:07 +0400

    Second part

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -1,51 +1,70 @@
 /**
  * @file Домашка по FP ч. 2
- * 
+ *
  * Подсказки:
  * Метод get у инстанса Api – каррированый
  * GET / https://animals.tech/{id}
- * 
+ *
  * GET / https://api.tech/numbers/base
  * params:
  * – number [Int] – число
  * – from [Int] – из какой системы счисления
  * – to [Int] – в какую систему счисления
- * 
+ *
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
 import Api from '../tools/api';
+import {andThen, ifElse, length, otherwise, pipe, prop, tap, test} from "ramda";
 
 const api = new Api();
 
-/**
- * Я – пример, удали меня
- */
-const wait = time => new Promise(resolve => {
-    setTimeout(resolve, time);
-})
 
 const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-    /**
-     * Я – пример, удали меня
-     */
-    writeLog(value);
 
-    api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-        writeLog(result);
-    });
+    const convertNumber = number => api.get('https://api.tech/numbers/base', {from: 10, to: 2, number})
+    const getAnimal = number => api.get(`https://animals.tech/${number}`, {})
 
-    wait(2500).then(() => {
-        writeLog('SecondLog')
+    const square = x => x * x
+    const modulo3 = x => x % 3
 
-        return wait(1500);
-    }).then(() => {
-        writeLog('ThirdLog');
+    const processConvertedNumber = pipe(
+        prop('result'),
+        tap(writeLog),
+        String,
+        length,
+        tap(writeLog),
+        square,
+        tap(writeLog),
+        modulo3,
+        tap(writeLog),
+        getAnimal,
+        andThen(pipe(
+            prop('result'),
+            handleSuccess
+        )),
+        otherwise(handleError),
+    )
 
-        return wait(400);
-    }).then(() => {
-        handleSuccess('Done');
-    });
+    const processNumber = pipe(
+        Math.round,
+        tap(writeLog),
+        convertNumber,
+        andThen(processConvertedNumber),
+        otherwise(handleError),
+    )
+
+
+    const app = pipe(
+        tap(writeLog),
+        ifElse(
+            test(/^[0-9\.]{3,9}$/),
+            processNumber,
+            handleError
+        ),
+    )
+
+    return app(value)
 }
 
 export default processSequence;
